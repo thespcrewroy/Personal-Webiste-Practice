6c350b0b727007d36e1db38755fd9f8e038b7acd
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-01-27 18:26:14 -0500

    Log id, display on console, and store it

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    // Sections active class (on-click, console-log button element)
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;     // target the name of data-id from HTML and put it into a var 'id'
+        if id {
+            // Remove selected from the other buttons
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            // Whatever id is coming from the dataset is assigned to the element varaible
+            const element = document.getElementById(id);
+        }
+    })
 }
 
 PageTransitions();
