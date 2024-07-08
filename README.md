482ce92202f667693e3c135c685e7495b848a129
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-07-07 23:28:10 -0400

    Finish Personal Website Tutorial

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
 # Personal-Webiste
 
-Currently @ 51:25 of the video: https://www.youtube.com/watch?v=xV7S8BhIeBo&t=995s
-
 Tasks:
+
 - Personalize the code to fit my style/what I want
 - THEN continue on with teh tutorial to finish "Header Content" Section
 
-- Do a tutorial on React, Typescript, javascript, HTML, SCSS, and CSS before continuing
+- Do a tutorial on React, Typescript, Javascript, SCSS, and CSS before continuing
 - After this, finish the makeathon-2 project and AuthFit project before doing other personal project ideas
 
 Sections
-* Blog Sections
-* Podcast Section
-* YouTube Section
-* Merch
-* Accessories
-* Patreon
-* Latest Video
-* Contact Me/Social Medias
-* Business Ventures
-* Portfolio Section
-* Portfolio Website on Medium
-* Portfolio Website on Codefoli
-* Research and Publications
-* Other Random Sections
 
+- Blog Sections
+- Podcast Section
+- YouTube Section
+- Merch
+- Accessories
+- Patreon
+- Latest Video
+- Contact Me/Social Medias
+- Business Ventures
+- Portfolio Section
+- Portfolio Website on Medium
+- Portfolio Website on Codefoli
+- Research and Publications
+- Other Random Sections
--- app.js
+++ app.js
@@ -1,39 +1,13 @@
-const sections = document.querySelectorAll('.section');     // Select all elements that shares a class of "section"
-const sectBtns = document.querySelectorAll('.controls');    // Select section buttons
-const sectBtn = document.querySelectorAll('.control');      // Select each individual button with a class of 'control'
-const allSections = document.querySelector('.main-content');// Select the body as a class of 'main content'
-
-
-function PageTransitions(){
-    // Button click active class (on-click, button becomes active button and turns green)
-    for(let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        })
-    }
-
-    // Sections active class (change active id to the element that was clicked)
-    allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;     // target the name of data-id from HTML and put it into a var 'id'
-        if (id) {
-            // Remove selected from the other buttons
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-            // hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-            })
-
-            // Whatever id is coming from the dataset is assigned to the element varaible
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
-}
-
-PageTransitions();
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+})();
